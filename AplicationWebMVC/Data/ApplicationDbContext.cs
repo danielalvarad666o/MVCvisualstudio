2a65132388df6a9243d3c03b0402d80fdb1dbe27
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2024-03-27 22:27:26 -0600

    MVC

---
+++ AplicationWebMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using App.Models;
+    
+    
+   
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Customers> customers { get; set; }
+     
+
+            [DbFunction(Schema = "dbo")]
+            public static int fn_PorductCategory_count(int pCategoryId)
+            {
+                throw new Exception();
+            }
+        }
+    }
+
+
